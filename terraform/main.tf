03a22e5970e262d1b8ba5a0671472ac749f26369
Almog ,Eden <edencaliber@gmail.com>
2026-04-03 14:14:49 +0300

    put networking module in root main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+module "networking" {
+  source = "./modules/networking"
+
+  project_name         = var.project_name
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = var.availability_zones
+  private_subnet_cidrs = var.private_subnet_cidrs
+  public_subnet_cidrs  = var.public_subnet_cidrs
+}
