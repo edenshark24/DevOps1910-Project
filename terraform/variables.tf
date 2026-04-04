4cc950863740a4d8330677def0f859eb0ba95352
Almog ,Eden <edencaliber@gmail.com>
2026-04-04 20:36:35 +0300

    added allowed ip range for ssh into variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,3 +36,8 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
+
+variable "allowed_ssh_cidrs" {
+  description = "List of CIDR blocks allowed to SSH into Jenkins"
+  type        = list(string)
+
