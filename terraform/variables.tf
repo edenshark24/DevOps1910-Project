eac322ebd15d543fcc26c298fdd657bf2f0c82b7
Almog ,Eden <edencaliber@gmail.com>
2026-04-11 21:17:24 +0300

    added db vars for root variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -45,3 +45,20 @@ variable "allowed_ssh_cidrs" {
   description = "AWS Key Pair name for SSH access"
   type        = string
 }
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
